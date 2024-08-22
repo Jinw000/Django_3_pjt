17a709ffc38e18c700844c7e983e85f60e561933
손진우 <sonjinwoo0729@gmail.com>
2024-08-22 12:23:03 +0900

    feat:회원기능 1차 완성

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST, require_http_methods
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+
+
 @require_http_methods(['GET', 'POST'])
 def signup(request):
     if request.method == 'POST':
@@ -15,8 +18,9 @@ def signup(request):
             return redirect('login')
     else:
         form = CustomUserCreationForm()
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'accounts/signup.html', context)
+
 
 @require_http_methods(['GET', 'POST'])
 def login(request):
@@ -24,11 +28,19 @@ def login(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('products/products.html')
+            next_url = request.GET.get("next") or "products:products"
+            return redirect(next_url)
     else:
         form = AuthenticationForm()
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'accounts/login.html', context)
+
 
 def home(request):
     return render(request, 'accounts/home.html')
+
+@require_POST
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect("home")
