b458929262e9b5a88a931eed2711566a7f6a47fb
gr22nsky <gr2nsky@gmail.com>
2024-08-22 09:59:55 +0900

    feat:회원기능1차시도

--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.urls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.login, name='login'),
     path("accounts/", include("accounts.urls")),
     path("products/", include("products.urls")),
 ]
