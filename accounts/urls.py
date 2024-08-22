134d06a2ebd57b4be245d6c4af9ecd52fd316d25
t2023-m0010 <t2023-m0010@t2023-m0010ui-MacBookAir.local>
2024-08-22 20:24:56 +0900

    feat: 회원탈퇴 기능 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('signout/', views.signout, name='signout'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -44,3 +44,10 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect("home")
+
+@require_POST
+def signout(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+        auth_logout(request)
+    return redirect("home")
